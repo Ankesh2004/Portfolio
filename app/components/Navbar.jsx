3bfadf754af4727c8a37404ba7ba2934c91cf400
Ankesh2004 <gankesh808@gmail.com>
2024-03-26 13:23:20 +0530

    feat:background adn particles

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import { useState } from "react";
+import Link from "next/link";
 import "./styles/Navbar.css";
 
 let tabs = [
@@ -15,7 +16,7 @@ const  Navbar = ()=>{
   let [activeTab, setActiveTab] = useState(tabs[0].id);
 
   return (
-    <div className="flex flex-wrap space-x-1 border-2 p-1 rounded-[2rem]">
+    <div className="flex z-0 flex-wrap space-x-1 border-2 p-1 rounded-[2rem]">
       {tabs.map((tab) => (
         <button
         key={tab.id}
@@ -25,17 +26,20 @@ const  Navbar = ()=>{
         } relative rounded-full px-3 py-1.5 text-sm font-medium text-white outline-sky-400 transition focus-visible:outline-2`}
         style={{
           WebkitTapHighlightColor: "transparent",
+          position: "relative" 
         }}
       >
+        <Link href={`#${tab.id}`}>
           {activeTab === tab.id && (
             <motion.span
               layoutId="bubble"
-              className={`absolute inset-0 z-10 bg-white mix-blend-difference`}
+              className={`absolute inset-0 z-50 bg-white mix-blend-difference`}
               style={{ borderRadius: 9999 }}
-              transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+              transition={{ type: "spring", bounce: 0.25, duration: 0.6 }}
             />
           )}
           {tab.label}
+        </Link>
         </button>
       ))}
     </div>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import 'next-google-fonts'
 import '@fontsource/fira-code'
 import Navbar from './components/Navbar';
+import HexagonBackground from './components/HexagonBackground';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <Navbar />
+      <HexagonBackground />
       <main >
 
       </main>
